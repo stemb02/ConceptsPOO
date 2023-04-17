1314553091efcfa54a2a0ad268160874de5780d1
Emiliano Stemberger <emilianostemberger123@gmail.com>
2023-04-16 21:30:46 -0300

    Class "Date" Finished

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("ConceptsPOO");
+Console.WriteLine("===========");
+
+try
+{
+	Console.WriteLine(new Date(2024, 1, 31));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 30));
+
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);	
+}
+
+
+
